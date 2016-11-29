54eb694e037000fe4062e615e8f8cb9bba28e7fc
Tim Burt <timcantango@gmail.com>
2016-11-28 21:25:35 -0600

    Initial commit

---
+++ README.rst
@@ -0,0 +1,42 @@
+Student Retainer
+================
+Tim Burt, Melie Lewis, Koby Pascual, Yutian Tang
+
+Description
+-----------
+
+Python/R-based program with GUI that uses data mining techniques (clustering/classification) to predict success/failure trends of
+students based on course choices along with retention information.
+●	Identify common course taking patterns for first-time, full-time freshmen.
+●	Predict student retention based on the identified course clusters and pre-existing risk factors.
+●	Tools that can be used by academic advisors to better aid in enrollment decisions.
+
+This program resulted from a class project in CS 5593 Data Mining from Fall 2016 at the University of Oklahoma.
+
+Installation
+------------
+
+
+Usage
+-----
+
+
+Missing data
+____________
+
+
+
+References
+----------
+
+.. [HUANG97] Huang, Z.: Clustering large data sets with mixed numeric and
+   categorical values, Proceedings of the First Pacific Asia Knowledge
+   Discovery and Data Mining Conference, Singapore, pp. 21-34, 1997.
+
+.. [HUANG98] Huang, Z.: Extensions to the k-modes algorithm for clustering
+   large data sets with categorical values, Data Mining and Knowledge
+   Discovery 2(3), pp. 283-304, 1998.
+
+.. [CAO09] Cao, F., Liang, J, Bai, L.: A new initialization method for
+   categorical data clustering, Expert Systems with Applications 36(7),
+   pp. 10223-10228., 2009.
---
+++ run.py
@@ -0,0 +1,99 @@
+# Script to generate knee plot from k-modes
+# Tim Burt
+# CS5593 project
+
+import csv
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+import kmodes
+
+# options
+max_clusters = 20
+save_dir = 'examples'
+# initialize variables
+classes_dict = {}
+class_counter = 0
+line_counter = 0
+dup_class_counter = 0
+filename = 'cleaned_data.csv'
+#
+classes_dict['.'] = 0  # force that as unknown class
+os.chdir(save_dir)
+file = open(filename)
+row_count = len(file.readlines())
+colm_count = 7
+with open(filename, 'rb') as data:
+    alllines = csv.reader(data, dialect='excel')
+    classes_arr = np.zeros((row_count, colm_count))  # initialize array for feeding to k-modes
+    for line in alllines:
+        for i in range(len(line)):
+            test_class = line[i]
+            if test_class != '.':
+                for j in range(len(line)):
+                    if i > j:  # loop over only once
+                        check_class = line[j]
+                        if (test_class == check_class):
+                            dup_class_counter += 1
+                            #print 'Found duplicate class in one record, line %d' % line_counter
+                            line[j] = '.'  # unknown class
+        for i in range(len(line)):
+            cur_class = line[i]
+            if cur_class not in classes_dict:  # add unique item to dict.
+                class_counter += 1
+                classes_dict[cur_class] = class_counter
+                classes_arr[line_counter][i] = class_counter
+            else:  # already in there
+                classes_arr[line_counter][i] = classes_dict[cur_class]
+        line_counter += 1
+print 'I found %d duplicate classes in one record' % dup_class_counter
+inv_classes_dict = {v: k for k, v in classes_dict.iteritems()}  # inversion of dictionary, for index lookup
+#print classes_dict
+#print classes_arr
+# Feed to k-modes, generate knee plot
+num_cluster_list = range(1, max_clusters + 1)
+cost = []
+centroids = []
+labels = []
+for i in num_cluster_list:
+    print "On cluster %d out of %d" % (i, max_clusters)
+    km = kmodes.KModes(n_clusters=i, init='Cao', n_init=25, verbose=0)
+    clusters = km.fit_predict(classes_arr)
+    centroids_temp = km.cluster_centroids_
+    cost_temp = km.cost_
+    labels_temp = km.labels_
+    cost.append(cost_temp)
+    centroids.append(centroids_temp)
+    labels.append(labels_temp)
+plt.plot(num_cluster_list, cost)
+plt.xlabel('Number of clusters')
+plt.ylabel('Cost')
+plt.savefig('knee_plot.pdf')
+plt.show()
+# print centroids for k choice and export
+actual_k = raw_input("Enter your k choice from the knee plot: ")
+actual_k = int(actual_k)
+print "Centroids for choice k=%d, exporting to centroids.txt:" % actual_k
+actual_centroids = centroids[actual_k - 1]
+# convert indices back to actual classes
+centroid_classes = []
+for i in range(len(actual_centroids)):
+    centroid_classes.append([])
+    for j in range(len(actual_centroids[i])):
+        cur_index = actual_centroids[i][j]
+        class_temp = inv_classes_dict[cur_index]
+        centroid_classes[i].append(class_temp)
+print actual_centroids
+print centroid_classes
+with open("centroids.txt", 'w') as data:
+    for i in range(len(centroid_classes)):
+        line = str(centroid_classes[i])
+        data.write(line)
+        data.write('\n')
+
+
+
+
+
+
